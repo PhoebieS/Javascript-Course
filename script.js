f245d4e693579efa2541afabc16f4a1beca83b6e
Slotcore <90641525+PhoebieS@users.noreply.github.com>
2021-10-19 09:32:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+/*
+let js = 'amazing';
+console.log(493 + 5893 + 3);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 4589;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 20;
+age = 21;
+
+const birthYear = 2000
+// const birthYear = 1999 (const can't be mutated.) while let can be mutated (changed)
+// const job; V-----V (Down)
+// Uncaught SyntaxError: Missing initializer in const declaration: meaning we'll need an initial value when just using const job meaning you should practice using const by default, and if you're sure you'll need to change use let.
+
+var job = 'entrepeneur';
+job = 'programmer'
+
+lastName = 'Hill';
+console.log(lastName)
+
+
+const now = 2050;
+const ageHill = now - 2005;
+const ageOthen = now - 1999;
+// I made it more simple, by adding a const var and instead of - whatever time I -'d it by now.
+console.log(ageHill, ageOthen);
+// you can lost more with a coma.
+//using - operator to solve simple problem.
+
+console.log(ageHill * 2, ageHill / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// added a comma
+
+//90 4.5 8 90 means x 2, 4.5 means divided by 10, and 90 means 2 to the power of 3.
+
+// Math Operators
+const firstName = 'Odin';
+const lastName = 'Hill';
+
+console.log(firstName + ' ' + lastName)
+// added an empty string to add a space to my code, which sent OdinHill and not Odin "Space " Hill.
+
+// Assignment operators
+let x = 20 + 7; // 15
+//two operators, =, and + .
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 148
+x++; // x = x + 1 = 149
+x--; // x = x - 1 = 147
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageHill > ageOthen); // false because ageOthen is greater than ageOdin (logged as false, instead of true and vise versa for which ever order.) >, M, >=, <=
+console.log(ageHill >= 18) // true because I've told the program im younger in the program due to the date I gave the variable (2005)
+// variables are all available to the console tab in chrome.
+
+const isFullAge = ageHill >= 18;
+
+console.log(now - 2005 > now - 1999)
+
+*/
+
+const now = 2050;
+const ageHill = now - 2005;
+const ageOthen = now - 1999;
+
+console.log(now - 2005 < now - 1999)
